32d422e1f65894512da14ee6ab6894c95581f552
Bernhard Schmitt <schmitt@sitegeist.de>
2023-12-18 22:33:05 +0100

    Improce factory usage

--- Classes/Infrastructure/FactoryFactory.php
+++ Classes/Infrastructure/FactoryFactory.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 namespace Sitegeist\SchemeOnYou\Infrastructure;
 
+use ApiPlatform\JsonSchema\Metadata\Property\Factory\SchemaPropertyMetadataFactory;
 use ApiPlatform\JsonSchema\SchemaFactory;
+use ApiPlatform\JsonSchema\TypeFactory;
 use ApiPlatform\Metadata\Property\Factory\DefaultPropertyMetadataFactory;
+use ApiPlatform\Metadata\Property\Factory\PropertyInfoPropertyMetadataFactory;
 use ApiPlatform\Metadata\Property\Factory\PropertyInfoPropertyNameCollectionFactory;
+use ApiPlatform\Metadata\Resource\Factory\AttributesResourceNameCollectionFactory;
 use ApiPlatform\Metadata\Resource\Factory\ResourceMetadataCollectionFactoryInterface;
+use ApiPlatform\Metadata\ResourceClassResolver;
 use Neos\Flow\Annotations as Flow;
 use Symfony\Component\PropertyInfo\Extractor\PhpDocExtractor;
 use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
@@ -24,18 +29,24 @@ class FactoryFactory
     public function createSchemaFactory(): SchemaFactory
     {
         $extractor = new ReflectionExtractor();
+        $propertyInfoExtractor = new PropertyInfoExtractor(
+            [$extractor],
+            [$extractor],
+            [new PhpDocExtractor()],
+            [$extractor],
+            [$extractor],
+        );
 
         return new SchemaFactory(
             null,
             $this->metadataCollectionFactory,
-            new PropertyInfoPropertyNameCollectionFactory(new PropertyInfoExtractor(
-                [$extractor],
-                [$extractor],
-                [new PhpDocExtractor()],
-                [$extractor],
-                [$extractor],
-            )),
-            new DefaultPropertyMetadataFactory()
+            new PropertyInfoPropertyNameCollectionFactory($propertyInfoExtractor),
+            new SchemaPropertyMetadataFactory(
+                new ResourceClassResolver(
+                    new AttributesResourceNameCollectionFactory([])
+                ),
+                new PropertyInfoPropertyMetadataFactory($propertyInfoExtractor)
+            )
         );
     }
 }
