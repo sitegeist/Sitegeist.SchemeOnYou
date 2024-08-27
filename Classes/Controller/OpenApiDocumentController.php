c86837816b1bb94b4dffaa4a47cac6e265f4e992
Bernhard Schmitt <schmitt@sitegeist.de>
2024-08-27 11:33:37 +0200

    Make setting the CORS header optional

--- Classes/Controller/OpenApiDocumentController.php
+++ Classes/Controller/OpenApiDocumentController.php
@@ -16,11 +16,16 @@ class OpenApiDocumentController extends ActionController
     #[Flow\Inject]
     protected OpenApiDocumentRepository $documentRepository;
 
+    #[Flow\InjectConfiguration(path: 'sendCORSHeaders')]
+    protected bool $sendCORSHeaders;
+
     public function renderAction(string $name): string
     {
         $schema = $this->documentRepository->findDocumentByName($name);
         $this->response->setContentType('application\json');
-        $this->response->addHttpHeader('Access-Control-Allow-Origin', '*');
+        if ($this->sendCORSHeaders) {
+            $this->response->addHttpHeader('Access-Control-Allow-Origin', '*');
+        }
         return \json_encode(
             $schema,
             JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE
