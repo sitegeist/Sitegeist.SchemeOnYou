4080187b8e4cd5d6ac3e0e56c8562b415d9eadde
Bernhard Schmitt <schmitt@sitegeist.de>
2024-12-09 18:37:31 +0100

    Make CORS header optional and adjust to beta 16

--- Classes/Controller/OpenApiDocumentController.php
+++ Classes/Controller/OpenApiDocumentController.php
@@ -6,21 +6,23 @@ namespace Sitegeist\SchemeOnYou\Controller;
 
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\Mvc\Controller\ActionController;
-use Neos\Flow\Mvc\View\JsonView;
 use Sitegeist\SchemeOnYou\Domain\OpenApiDocumentRepository;
 
 class OpenApiDocumentController extends ActionController
 {
-    protected $defaultViewObjectName = JsonView::class;
-
     #[Flow\Inject]
     protected OpenApiDocumentRepository $documentRepository;
+
+    #[Flow\InjectConfiguration(path: 'sendCORSHeaders')]
+    protected bool $sendCORSHeaders;
 
     public function renderAction(string $name): string
     {
         $schema = $this->documentRepository->findDocumentByName($name);
         $this->response->setContentType('application\json');
-        $this->response->addHttpHeader('Access-Control-Allow-Origin', '*');
+        if ($this->sendCORSHeaders) {
+            $this->response->addHttpHeader('Access-Control-Allow-Origin', '*');
+        }
         return \json_encode(
             $schema,
             JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE
