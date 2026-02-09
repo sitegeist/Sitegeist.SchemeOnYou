f1b8a54bae2062bf1ceaec0547b452e7b12bd6ee
Martin Ficzel <ficzel@sitegeist.de>
2026-02-09 15:24:30 +0100

    TASK: Allow Interfaces and UnionTypes of DTOs in Collections and adjust DTO Specification Objects accordingly

---
+++ Tests/Fixtures/Stuff/BoringStuff.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Sitegeist\SchemeOnYou\Tests\Fixtures\Stuff;
+
+use Neos\Flow\Annotations as Flow;
+
+#[Flow\Proxy(false)]
+readonly class BoringStuff implements StuffInterface
+{
+    public function __construct(
+        public int $number
+    ) {
+    }
+}
---
+++ Tests/Fixtures/Stuff/CollectionOfStuffViaInterface.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace Sitegeist\SchemeOnYou\Tests\Fixtures\Stuff;
+
+use Neos\Flow\Annotations as Flow;
+
+#[Flow\Proxy(false)]
+readonly class CollectionOfStuffViaInterface
+{
+    /**
+     * @var StuffInterface[]
+     */
+    public array $items;
+
+    public function __construct(StuffInterface ... $items) {
+        $this->items = $items;
+    }
+}
